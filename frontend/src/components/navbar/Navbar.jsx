1f23a5a5b965130bb15084f7f66f9832c649fcde
chandra <chandral@elderda.co.uk>
2025-03-19 09:25:41 +0000

    Sorted the continue with email button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
-
 const Navbar = ({ showButtons = true }) => {
   const navigate = useNavigate();
 
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -19,27 +19,22 @@ const Login = () => {
     <>
       <Navbar showButtons={false} />
       <div className="loginContainer">
-        <div className="loginBox">
-          <h1 className="loginTitle">Login to access your account</h1>
-          <form onSubmit={handleSubmit} className="loginForm">
-            <div className="formGroup">
-              <label htmlFor="email" className="formLabel">Email address</label>
-              <br /> {/* Add a line break to move the input below the label */}
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-                className="formInput"
-                placeholder="Enter your email address"
-                required
-              />
-            </div>
-            
-          </form>
-          <button type="submit" className="loginButton">Continue with email</button>
-        </div>
+        <h1 className="loginTitle">Login to access your account</h1>
+        <form onSubmit={handleSubmit} className="loginForm">
+            <label htmlFor="email" className="formLabel">Email address</label>
+            <br /> {/* Add a line break to move the input below the label */}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              className="formInput"
+              placeholder="Enter your email address"
+              required
+            />
+            <button type="submit" className="loginButton">Continue with email</button>
+        </form>
       </div>
     </>
   );
